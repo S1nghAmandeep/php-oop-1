7f787b6df8325a058b36bc319bbaf1cb7bdbf454
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-28 16:38:02 +0100

    add get function / add stars to rating vote

--- index.php
+++ index.php
@@ -41,6 +41,21 @@ class Production
             $this->rating = 'null';
         }
     }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+
+    public function getRating()
+    {
+        return $this->rating;
+    }
 }
 
 $marvel = new Production('Thor', 'Italiano', 4);
@@ -59,24 +74,46 @@ $movies = [
 
 ?>
 
-<div>
-    <table>
-        <thead>
-            <tr>
-                <th>Film</th>
-                <th>Language</th>
-                <th>Rating</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($movies as $film) { ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie list</title>
+</head>
+
+<body>
+    <div>
+        <table>
+            <thead>
                 <tr>
-                    <td><?php echo $film->title ?></td>
-                    <td><?php echo $film->language ?></td>
-                    <td><?php echo $film->rating ?></td>
+                    <th>Film</th>
+                    <th>Language</th>
+                    <th>Rating</th>
                 </tr>
-            <?php
-            } ?>
-        </tbody>
-    </table>
-</div>
+            </thead>
+            <tbody>
+                <?php foreach ($movies as $film) { ?>
+                    <tr>
+                        <td><?php echo $film->title ?></td>
+                        <td><?php echo $film->language ?></td>
+                        <td>
+                            <?php for ($i = 0; $i < $film->rating; $i++) { ?>
+                                &starf;
+                            <?php
+                            }
+                            for ($i = 0; $i < 5 - $film->rating; $i++) { ?>
+                                &star;
+                            <?php
+                            } ?>
+                        </td>
+                    </tr>
+                <?php
+                } ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
