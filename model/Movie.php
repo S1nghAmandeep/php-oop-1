dadde76d89e848ebf0546425826a5e0d61b3460f
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-29 16:08:45 +0100

    done movie class

--- model/Movie.php
+++ model/Movie.php
@@ -4,4 +4,44 @@ require_once __DIR__ . './Production.php';
 
 class Movie extends Production
 {
+    private $profit;
+    private $duraction;
+
+    function __construct($_title, $_language, $_rating, $_duraction, $_profit)
+    {
+
+        parent::__construct($_title, $_language, $_rating);
+        // $this->profit = $_profit;
+        $this->setProfit($_profit);
+        // $this->duraction = $_duraction;
+        $this->setDuraction($_duraction);
+    }
+
+    public function setProfit($profit)
+    {
+        if (is_string($profit)) {
+            $this->profit = $profit;
+        } else {
+            $this->profit = null;
+        }
+    }
+
+    public function setDuraction($duraction)
+    {
+        if (is_string($duraction)) {
+            $this->duraction = $duraction;
+        } else {
+            $this->duraction = null;
+        }
+    }
+
+    public function getProfit()
+    {
+        return $this->profit;
+    }
+
+    public function getDuraction()
+    {
+        return $this->duraction;
+    }
 }
