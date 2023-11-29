56b80d89ae1fc8c4f87e2eb075b029dcf6758490
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-29 16:20:24 +0100

    add 5 movie/series

--- index.php
+++ index.php
@@ -1,9 +1,37 @@
 <?php
 
 
-require_once __DIR__ . './model/Production.php'
+require_once __DIR__ . './model/Production.php';
+require_once __DIR__ . './model/Movie.php';
+require_once __DIR__ . './model/Series.php';
 
-// var_dump($movies);
+$marvel = new Movie('Thor', 'Italiano', 4, '2h', '100M');
+$dc = new Movie('Batman', 'Italiano', 3, '3h', '50M');
+$disney = new Movie('Toy Story', 'Italiano', 5, '1.5h', '200M');
+$iron_Man = new Movie('Iron Man', 'ita', 5, '2h', '200M');
+$titanic = new Movie('Titanic', 'ita', 5, '3h', '500M');
+$suits = new Series('Suits', 'Ita', 4, 10);
+$loki = new Series('Loki', 'ita', 4, 2);
+$friends = new Series('Friends', 'ita', 3, 15);
+$build = new Series('Build', 'ita', 3, 10);
+$time = new Series('Time', 'ita', 2, 1);
+
+var_dump($marvel, $dc, $suits, $loki);
+
+$movies = [
+    $marvel,
+    $dc,
+    $disney,
+    $iron_Man,
+    $titanic,
+    $suits,
+    $loki,
+    $friends,
+    $build,
+    $time,
+];
+
+
 
 ?>
 
